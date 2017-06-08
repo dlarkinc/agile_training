8bd7c41cff33ea20c3e94086f01074f7884910ed
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-08 17:10:18 +0100

    change x to 5

--- src/power.py
+++ src/power.py
@@ -7,7 +7,7 @@ def to_the_power(x, y = 2):
         result = result * x
     return result
 
-x = 3
+x = 5
 y = 8 # 
 print("{0} to the power of {1} is {2}".format(x, y, to_the_power(x, y)))
 print("The End")
