84a8091856ff15030135189c6639050cab78355f
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-08 16:53:02 +0100

    Added an end line

--- src/power.py
+++ src/power.py
@@ -5,7 +5,7 @@ def to_the_power(x, y = 2):
         result = result * x
     return result
 
-x = 11
-y = 4
+x = 3
+y = 8
 print("{0} to the power of {1} is {2}".format(x, y, to_the_power(x, y)))
-
+print("The End")
