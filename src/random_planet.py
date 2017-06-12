ab9ccf71b04c621e96b96ab548096243a695b487
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-12 16:03:06 +0100

    added the moon to the planets

--- src/random_planet.py
+++ src/random_planet.py
@@ -1,5 +1,5 @@
 import random
 
-planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Moon']
 
 print("One of the planets in our solar system is {0}".format(random.choice(planets)))
