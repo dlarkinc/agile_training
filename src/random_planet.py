77451eccba1f96e249cd5cda3256ee8a397b3464
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-12 16:11:48 +0100

    added Titan to the planets

--- src/random_planet.py
+++ src/random_planet.py
@@ -1,5 +1,5 @@
 import random
 
-planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Moon']
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Moon', 'Titan']
 
 print("One of the planets in our solar system is {0}".format(random.choice(planets)))
